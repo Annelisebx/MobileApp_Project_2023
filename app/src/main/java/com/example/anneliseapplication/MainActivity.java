692fbfb5d2860b2397002dd6706b6a1f9cd45b37
Annelisebx <50610153+Annelisebx@users.noreply.github.com>
2023-05-09 02:01:55 -0700

    Calm Shiba and Angry Shiba

--- app/src/main/java/com/example/anneliseapplication/MainActivity.java
+++ app/src/main/java/com/example/anneliseapplication/MainActivity.java
@@ -7,31 +7,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button food_button;
+    Button touch_button;
+    ImageView iv;
+    int[] images ={R.drawable.angry_shiba,R.drawable.calm_shiba};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.touch_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        iv=(ImageView) findViewById(R.id.angry_shiba);
+        touch_button=(Button) findViewById(R.id.touch_button);
+        food_button=(Button) findViewById(R.id.food_button);
 
-                // Code here executes on main thread after user presses button
+        touch_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv.setImageResource(images[0]);
+                toastMsg("Bark :(");
             }
         });
 
+        food_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv.setImageResource(images[1]);
+                toastMsg("Yummy :)");
+            }
+        });
     }
     public void toastMsg(String text) {
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
         toast.show();
     }
 
-    public void displayToastMsg(View view) {
-        toastMsg("Toast!!!");
+    public void displayFoodMsg(View view) {
+        toastMsg("Yummy :)");
     }
+
+    public void displayTouchMsg(View view) {
+        toastMsg("Bark :(");
+    }
+
 
 }
